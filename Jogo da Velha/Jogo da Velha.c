87c65a45003b015331c1d8df637b0970cc958835
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2019-10-26 21:27:23 -0300

    Verificador de vitória consertado, mas não implatado. Tentativas de fazer o supergridPrinter funcionar. Não houve sucesso, a cópia de array não está funcionando corretamente

--- Jogo da Velha/Jogo da Velha.c
+++ Jogo da Velha/Jogo da Velha.c
@@ -5,6 +5,9 @@
 
 #define TRUE 1
 
+/*verifica se alguém venceu, caso X vence, retorna 1
+Caso O vence, retorna -1
+caso não haja vitória, retorna 0*/
 int winVerifyer(int grid[3][3])
 {
 	if (grid[0][0] == 1)
@@ -32,10 +35,37 @@ int winVerifyer(int grid[3][3])
 		return 1;
 	if (grid[2][0] == 1 && grid[2][1] == 1 && grid[2][2] == 1)
 		return 1;
+
+	if (grid[0][0] == -1)
+	{
+		if (grid[0][1] == -1 && grid[0][2] == -1)
+			return -1;
+		if (grid[1][0] == -1 && grid[2][0] == -1)
+			return -1;
+		if (grid[1][1] == -1 && grid[2][2] == -1)
+			return -1;
+	}
+	if (grid[0][1] == -1)
+	{
+		if (grid[1][1] == -1 && grid[2][1] == -1)
+			return -1;
+	}
+	if (grid[0][2] == -1)
+	{
+		if (grid[1][2] == -1 && grid[2][2] == -1)
+			return -1;
+		if (grid[1][1] == -1 && grid[2][0] == -1)
+			return -1;
+	}
+	if (grid[1][0] == -1 && grid[1][1] == -1 && grid[1][2] == -1)
+		return -1;
+	if (grid[2][0] == -1 && grid[2][1] == -1 && grid[2][2] == -1)
+		return -1;
 	else
 		return 0;
 }
 
+//converte o posicionamento de 1 a 9 por coordenadas
 int* convertToCoordinate(int position)
 {
 	static int coordinate[2];
@@ -88,6 +118,8 @@ int* convertToCoordinate(int position)
 	return coordinate;
 }
 
+//converte as coordenadas da grade para coordenadas da
+//super grade
 int* coordinatesToSupergrid(int coordinates[2])
 {
 	static int supergridCoordinates[2];
@@ -126,6 +158,7 @@ int* coordinatesToSupergrid(int coordinates[2])
 	return supergridCoordinates;
 }
 
+//modifica a super grade, botando X ou O
 void superGridModifier(char supergrid[26][46], int grid[3][3])
 {
 	int supergridCoordinates[2], gridCoordinates[2];
@@ -156,29 +189,30 @@ void superGridModifier(char supergrid[26][46], int grid[3][3])
 			memcpy(supergridCoordinates, coordinatesToSupergrid(gridCoordinates), sizeof(supergridCoordinates));
 			supergridX = supergridCoordinates[0];
 			supergridY = supergridCoordinates[1];
-			symbolX = 0;
+			symbolY = 0;
 			for (; supergridCoordinates[1] < supergridY + 8; supergridCoordinates[1]++)
 			{
-				symbolY = 0;
+				symbolX = 0;
 				for (; supergridCoordinates[0] < supergridX + 14; supergridCoordinates[0]++)
 				{
 					if (grid[gridCoordinates[0]][gridCoordinates[1]] == 1)
 						supergrid[supergridCoordinates[0]][supergridCoordinates[1]] = x[symbolX][symbolY];
 					if (grid[gridCoordinates[0]][gridCoordinates[1]] == -1)
 						supergrid[supergridCoordinates[0]][supergridCoordinates[1]] = o[symbolX][symbolY];
-					symbolY++;
+					symbolX++;
 				}
-				symbolX++;
+				symbolY++;
 			}
 		}
 	}
 
 }
 
+//imprime a grade
 void gridPrinter(int grid[3][3], int mod)
 {
 	static char superGrid[26][46];
-	//creates the supergrid
+	//cria a super grade
 	if (mod == 0)
 	{
 		for (int i = 0; i < 26; i++)
@@ -200,7 +234,7 @@ void gridPrinter(int grid[3][3], int mod)
 			}
 		}
 	}
-	//just prints the supergrid
+	//somente imprime a super grade
 	if (mod == 1)
 	{
 		for (int i = 0; i < 26; i++)
@@ -212,7 +246,7 @@ void gridPrinter(int grid[3][3], int mod)
 			}
 		}
 	}
-	//modifys the supergrid and print it
+	//chama a função que modifica a grade e imprime depois
 	if (mod == 2)
 	{
 		superGridModifier(superGrid, grid);
@@ -225,12 +259,12 @@ void gridPrinter(int grid[3][3], int mod)
 			}
 		}
 	}
-	//prints the grid with 1 to 9 in it
+	//imprime a grade com números de 1 a 9 nela
 	if (mod == 3)
 	{
 		printf("\n         88    ##   ad888888b, ##   ad888888b, \n       ,d88    ##  d8\"     \"88 ##  d8\"     \"88 \n     888888    ##          a8P ##          a8P \n         88    ##       ,d8P\"  ##       aad8\"  \n         88    ##     a8P\"     ##       \"\"Y8,  \n         88    ##   a8P\'       ##          \"8b \n         88    ##  d8\"         ##  Y8,     a88 \n         88    ##  88888888888 ##   \"Y888888P\' \n ##############################################\n          ,d8  ##  8888888888  ##    ad8888ba, \n        ,d888  ##  88          ##   8P\'    \"Y8 \n      ,d8\" 88  ##  88  ____    ##  d8          \n    ,d8\"   88  ##  88a8PPPP8b, ##  88,dd888bb, \n  ,d8\"     88  ##  PP\"     `8b ##  88P\'    `8b \n  8888888888888##           d8 ##  88       d8 \n           88  ##  Y8a     a8P ##  88a     a8P \n           88  ##   \"Y88888P\"  ##   \"Y88888P\"  \n ##############################################\n  888888888888 ##   ad88888ba  ##   ad88888ba  \n           8P\' ##  d8\"     \"8b ##  d8\"     \"88 \n         d8\"   ##  Y8a     a8P ##  8P       88 \n       ,8P\'    ##    \"Y8aaa8P\" ##  Y8,    ,d88 \n      d8\"      ##   ,d8\"\"\"8b,  ##   \"PPPPPP\"88 \n    ,8P\'       ##  d8\"     \"8b ##           8P \n   d8\"         ##  Y8a     a8P ##  8b,    a8P  \n  8P\'          ##   \"Y88888P\"  ##  `\"Y8888P\'   \n");
 	}
-	//prints the grid with (1,1) to (3,3)
+	//imprime a grade com as coordenadas de (1,1) até (3,3)
 	if (mod == 4)
 	{
 		printf("\n               ##              ##              \n               ##              ##              \n   /         \\ ##  /      __ \\ ##  /      __ \\ \n  ( /|    /|  )## ( /|     _) )## ( /|    __) )\n   \\ |  /  | / ##  \\ |  / /__/ ##  \\ |  / __)/ \n               ##              ##              \n               ##              ##              \n               ##              ##              \n ##############################################\n               ##              ##              \n               ##              ##              \n   /__       \\ ##  /__    __ \\ ##  /__    __ \\ \n  (  _)   /|  )## (  _)    _) )## (  _)   __) )\n   \\/__ /  | / ##  \\/__ / /__/ ##  \\/__ / __)/ \n               ##              ##              \n               ##              ##              \n               ##              ##              \n ##############################################\n               ##              ##              \n               ##              ##              \n   /__       \\ ##  /__    __ \\ ##  /__    __ \\ \n  ( __)   /|  )## ( __)    _) )## ( __)   __) )\n   \\__) /  | / ##  \\__) / /__/ ##  \\__) / __)/ \n               ##              ##              \n               ##              ##              \n               ##              ##              \n ");
@@ -247,15 +281,15 @@ int main(int argc, char* argv[])
 	printf("Iniciando Jogo da Velha!\n");
 	printf("Como gostaria o tabuleiro? (Alerta, muitas linhas! Pressione enter para continuar)\n");
 	system("pause");
-	printf("1: As posi��es v�o de 1 a 9\n");
+	printf("1: As posições vão de 1 a 9\n");
 	gridPrinter(grid, 3);
-	printf("\n2: As posi��es s�o coordenadas to tipo (x,y)\n");
+	printf("\n2: As posições são coordenadas to tipo (x,y)\n");
 	gridPrinter(grid, 4);
 	if (scanf("%i", &insertionPreference) == 1);
 	printf("Gostaria de jogar o modo: 1- Singleplyer ou 2- Multiplayer\n");
 	if (scanf("%i", &player) == 1);
 	gridPrinter(grid, 0);
-	printf("Com qual gostaria de jogar? 1-  ou 2- ");
+	printf("Com qual gostaria de jogar? 1- X ou 2- O\n");
 	if (scanf("%i", &symbolPreference) == 2);
 	if (symbolPreference == 2)
 		symbolPreference = -1;
