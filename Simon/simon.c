b7b9197beccaf5ec5ae96270c6201fdff40b3d17
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2019-11-21 13:51:09 -0300

    Simon: -Started creating the print function. Base working when lights off, need improvements -TO DO: fix the sequence os presses, because I'm dumb and forgot how simon works

--- Simon/simon.c
+++ Simon/simon.c
@@ -9,12 +9,62 @@ extern void restoreConsoleMode(void);
 extern void restoreConsole(void);
 extern void getCursorPosition(int* row, int* col);
 
+typedef unsigned char sshort;
+
 typedef enum ColorButtons {
 	GREEN = 1,
 	RED,
 	YELLOW,
 	BLUE
 }Color;
+
+void display(int button, bool zero, int screenSize[2])
+{
+	setupConsole();
+	if (zero == true)
+	{
+		moveTo(0, 0);
+		
+		for (int row = 0; row <= screenSize[0]/2 ; row++)
+		{
+			setBackgroundColorRGB(30, 50, 40); // shadow green
+			for (int greenCol = 0; greenCol <= (screenSize[1] / 2) - 1; greenCol++)
+				printf(" ");
+
+			if (screenSize[1] % 2 != 0)
+				printf(" ");
+
+			setBackgroundColorRGB(50, 30, 40); // shadow red
+			for (int rednCol = 0; rednCol <= (screenSize[1] / 2) - 1; rednCol++)
+				printf(" ");
+
+			printf("\n");
+		}
+
+		if (screenSize[0] % 2 != 0)
+			printf("\n");
+
+		for (int row = 0; row <= screenSize[0]/2; row++)
+		{
+			setBackgroundColorRGB(50, 50, 0); // shadow yellow
+			for (int greenCol = 0; greenCol <= (screenSize[1] / 2) - 1; greenCol++)
+				printf(" ");
+
+			if (screenSize[1] % 2 != 0)
+				printf(" ");
+
+			setBackgroundColorRGB(40, 30, 50); // shadow blue
+			for (int rednCol = 0; rednCol <= (screenSize[1] / 2) - 1; rednCol++)
+				printf(" ");
+
+			printf("\n");
+		}
+
+	}
+
+
+	restoreConsoleMode();
+}
 
 void rng(int* sequence, int level)
 {
@@ -42,26 +92,33 @@ int main(int argc, char** argv)
 {
 	int level = 2;
 	int windowSize[2];
-	int sequence[4000];
+	sshort sequence[4000];
 	char trashcan[10];
 
 	srand(time(NULL));
 
-	printf("Starting Simon. Do you wish to:\n(P) play now\nor \n (T)see the tutorial?\n");
+	printf("Starting Simon. Do you wish to:\n(P) play now\nor \n(T)see the tutorial?\n");
 	fgets(trashcan, 5, stdin);
 
-	if (trashcan[0] == 'T')
+	if (trashcan[0] == 'T' || trashcan[0] == 't')
 	{
 		//teach how to play
 	}
 
-	if (trashcan[0] == 'P')
+	if (trashcan[0] == 'P' || trashcan[0] == 'p')
 	{
 		for (bool replay = true; replay == true;)
 		{
-			clearScreenToBottom();
+			setupConsole();
+			moveTo(999, 999);
 			getCursorPosition(&windowSize[0], &windowSize[1]);
 			clearScreenToTop();
+			restoreConsoleMode();
+			display(1, true, windowSize);
+			resetColor();
+			break;
 		}
 	}
+
+	return 0;
 }
