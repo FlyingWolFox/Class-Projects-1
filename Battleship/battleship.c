ae22aab6717f2870db903e233f24333ef691f5a1
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2019-11-24 16:48:18 -0300

    Battleship: -Creation of the project -Basic project setup

---
+++ Battleship/battleship.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "ansi_escapes.h"
+
+#include "windowsConsoleInteraction.h"
+#include <conio.h>
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
