76e0a3ccd6f6f7d0020f96f9ecaf2014a883db6f
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2019-11-30 20:20:33 -0300

    Hangman commented and made the code little more clean

--- Forca/hangman.c
+++ Forca/hangman.c
@@ -4,16 +4,19 @@
 #include <string.h>
 #include <stdbool.h>
 
+// holds the word info
 typedef struct WordInfo {
 	char word[30];
 	int numberOfLetters;
 	char tip[40];
 }Word;
 
+// the coordinate system used
 typedef struct Coordinates {
 	int row, col;
 }Coordinate;
 
+// enum with the body parts to help code reading
 typedef enum BodyParts {
 	HANG,
 	HEAD,
@@ -25,111 +28,116 @@ typedef enum BodyParts {
 	RIGHT_LEG
 }Body;
 
+// cretes the letter Array
 void letterArrayCreator(char letters[28][3][5])
 {
+	// it has the ascii art of all letters
+	// it put all of them in th eletter array
+	// to be put in the display more easily
+
 	char a[3][5] = { {'_', '_', '_', '_', ' '},
-{'|', '_', '_', '|', ' '},
-{'|', ' ', ' ', '|', ' '} };
+					 {'|', '_', '_', '|', ' '},
+					 {'|', ' ', ' ', '|', ' '} };
 
 	char b[3][5] = { {'_', '_', '_', ' ', ' '},
-	{'|', '_', '_', ']', ' '},
-	{'|', '_', '_', ']', ' '} };
+					 {'|', '_', '_', ']', ' '},
+					 {'|', '_', '_', ']', ' '} };
 
 	char c[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', ' ', ' ', ' ', ' '},
-	{'|', '_', '_', '_', ' '} };
+					 {'|', ' ', ' ', ' ', ' '},
+					 {'|', '_', '_', '_', ' '} };
 
 	char d[3][5] = { {'_', '_', '_', ' ', ' '},
-	{'|', ' ', ' ', '\\', ' '},
-	{'|', '_', '_', '/', ' '} };
+					 {'|', ' ', ' ', '\\', ' '},
+					 {'|', '_', '_', '/', ' '} };
 
 	char e[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', '_', '_', '_', ' '},
-	{'|', '_', '_', '_', ' '} };
+					 {'|', '_', '_', '_', ' '},
+					 {'|', '_', '_', '_', ' '} };
 
 	char f[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', '_', '_', '_', ' '},
-	{'|', ' ', ' ', ' ', ' '} };
+					 {'|', '_', '_', '_', ' '},
+					 {'|', ' ', ' ', ' ', ' '} };
 
 	char g[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', ' ', '_', '_', ' '},
-	{'|', '_', '_', ']', ' '} };
+					 {'|', ' ', '_', '_', ' '},
+					 {'|', '_', '_', ']', ' '} };
 
 	char h[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{'|', '_', '_', '|', ' '},
-	{'|', ' ', ' ', '|', ' '} };
+					 {'|', '_', '_', '|', ' '},
+					 {'|', ' ', ' ', '|', ' '} };
 
 	char i[3][5] = { {' ', ' ', '_', ' ', ' '},
-	{' ', ' ', '|', ' ', ' '},
-	{' ', ' ', '|', ' ', ' '} };
+					 {' ', ' ', '|', ' ', ' '},
+					 {' ', ' ', '|', ' ', ' '} };
 
 	char j[3][5] = { {' ', '_', ' ', ' ', ' '},
-	 {' ', '|', ' ', ' ', ' '},
-	{'_', '|', ' ', ' ', ' '} };
+					 {' ', '|', ' ', ' ', ' '},
+					 {'_', '|', ' ', ' ', ' '} };
 
 	char k[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{'|', '_', '/', ' ', ' '},
-	{'|', ' ', '\\', '_', ' '} };
+					 {'|', '_', '/', ' ', ' '},
+					 {'|', ' ', '\\', '_', ' '} };
 
 	char l[3][5] = { {'_', ' ', ' ', ' ', ' '},
-	{'|', ' ', ' ', ' ', ' '},
-	{'|', '_', '_', '_', ' '} };
+					 {'|', ' ', ' ', ' ', ' '},
+					 {'|', '_', '_', '_', ' '} };
 
 	char m[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{'|', '\\', '/', '|', ' '},
-	{'|', ' ', ' ', '|', ' '} };
+					 {'|', '\\', '/', '|', ' '},
+					 {'|', ' ', ' ', '|', ' '} };
 
 	char n[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{'|', '\\', ' ', '|', ' '},
-	{'|', ' ', '\\', '|', ' '} };
+					 {'|', '\\', ' ', '|', ' '},
+					 {'|', ' ', '\\', '|', ' '} };
 
 	char o[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', ' ', ' ', '|', ' '},
-	{'|', '_', '_', '|', ' '} };
+					 {'|', ' ', ' ', '|', ' '},
+					 {'|', '_', '_', '|', ' '} };
 
 	char p[3][5] = { {'_', '_', '_', ' ', ' '},
-	{'|', '_', '_', ']', ' '},
-	{'|', ' ', ' ', ' ', ' '} };
+					 {'|', '_', '_', ']', ' '},
+					 {'|', ' ', ' ', ' ', ' '} };
 
 	char q[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', ' ', ' ', '|', ' '},
-	{'|', '_', '\\', '|', ' '} };
+					 {'|', ' ', ' ', '|', ' '},
+					 {'|', '_', '\\', '|', ' '} };
 
 	char r[3][5] = { {'_', '_', '_', '_', ' '},
-	{'|', '_', '_', '/', ' '},
-	{'|', ' ', ' ', '\\', ' '} };
+					 {'|', '_', '_', '/', ' '},
+					 {'|', ' ', ' ', '\\', ' '} };
 
 	char s[3][5] = { {'_', '_', '_', '_', ' '},
-	{'[', '_', '_', ' ', ' '},
-	{'_', '_', '_', ']', ' '} };
+					 {'[', '_', '_', ' ', ' '},
+					 {'_', '_', '_', ']', ' '} };
 
 	char t[3][5] = { {'_', '_', '_', ' ', ' '},
-	{' ', '|', ' ', ' ', ' '},
-	{' ', '|', ' ', ' ', ' '} };
+					 {' ', '|', ' ', ' ', ' '},
+					 {' ', '|', ' ', ' ', ' '} };
 
 	char u[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{'|', ' ', ' ', '|', ' '},
-	{'|', '_', '_', '|', ' '} };
+					 {'|', ' ', ' ', '|', ' '},
+					 {'|', '_', '_', '|', ' '} };
 
 	char v[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{'|', ' ', ' ', '|', ' '},
-	{' ', '\\', '/', ' ', ' '} };
+					 {'|', ' ', ' ', '|', ' '},
+					 {' ', '\\', '/', ' ', ' '} };
 
 	char w[3][5] = { {'_', ' ', '_', ' ', '_'},
-	{'|', ' ', '|', ' ', '|'},
-	{'|', '_', '|', '_', '|'} };
+					 {'|', ' ', '|', ' ', '|'},
+					 {'|', '_', '|', '_', '|'} };
 
 	char x[3][5] = { {'_', ' ', ' ', '_', ' '},
-	{' ', '\\', '/', ' ', ' '},
-	{'_', '/', '\\', '_', ' '} };
+					 {' ', '\\', '/', ' ', ' '},
+					 {'_', '/', '\\', '_', ' '} };
 
 	char y[3][5] = { {'_', ' ', ' ', ' ', '_'},
-	{' ', '\\', '_', '/', ' '},
-	{' ', ' ', '|', ' ', ' '} };
+					 {' ', '\\', '_', '/', ' '},
+					 {' ', ' ', '|', ' ', ' '} };
 
 	char z[3][5] = { {'_', '_', '_', ' ', ' '},
-	{' ', ' ', '/', ' ', ' '},
-	{' ', '/', '_', '_', ' '} };
+					 {' ', ' ', '/', ' ', ' '},
+					 {' ', '/', '_', '_', ' '} };
 
 	char _[3][5] = { {' ', ' ', ' ', ' ', ' '},
 					 {' ', ' ', ' ', ' ', ' '},
@@ -170,6 +178,9 @@ void letterArrayCreator(char letters[28][3][5])
 
 }
 
+// sees if the word received is in the word
+// if yes, return true
+// if no, returns false
 bool displayModifyer(Word word, Word* display, char letter)
 {
 	bool foundLetter = false;
@@ -184,8 +195,13 @@ bool displayModifyer(Word word, Word* display, char letter)
 	return foundLetter;
 }
 
+// verifies if the game ended
 int endVerifyer(char hangMan[8][11], Word display)
 {
+	// if aren't any empty letter sapeces
+	// or the hangman body is full
+	// it is because the game ende
+
 	int underlineCount = 0;
 	if (hangMan[5][10] == '\\')
 		return 2;
@@ -199,9 +215,11 @@ int endVerifyer(char hangMan[8][11], Word display)
 	return 0;
 }
 
+// copies the hang and the body parts when asked to
 void hangManCopyer(char hangMan[8][11], Body partToUpdate)
 {
 	/*
+	hangman:
 			 _______
 			|/      |
 			|      (_)
@@ -262,6 +280,7 @@ void hangManCopyer(char hangMan[8][11], Body partToUpdate)
 
 }
 
+// put the letter in the letterGrid to be displayed
 void letterCopier(char letterGrid[3][5], char letter, bool inicialization)
 {
 	static char letters[28][3][5];
@@ -278,21 +297,26 @@ void letterCopier(char letterGrid[3][5], char letter, bool inicialization)
 		memcpy(letterGrid, letters[27], sizeof(letters[27]));
 }
 
+// prints the screem, with the hangman, word and letters already picked
 void screenPrinter(char hangMan[8][11], char* tip, Word word, char* alreadyChoosenLetters)
 {
 	char letterGrid[3][5];
 
 	clearScreenToTop();
 	moveTo(0, 0);
+
+	// prints the top part of the hangman
 	for (int count = 0; count < 11; count++)
 	{
 		printf("%c", hangMan[0][count]);
 	}
 	moveRight(5);
 
+	// prints the tip at the top
 	printf("%s", word.tip);
 	moveTo(2, 0);
 
+	// prints the rest of the hangman
 	for (int i = 1; i < 8; i++)
 	{
 		for (int j = 0; j < 11; j++)
@@ -302,6 +326,9 @@ void screenPrinter(char hangMan[8][11], char* tip, Word word, char* alreadyChoos
 			printf("\n");
 	}
 
+	// print the letters
+	// it'll be ascii art letters if the word isn't bigger than 15
+	// if it's bigger, prints normal letters
 	moveRight(5);
 	if (word.numberOfLetters < 15)
 	{
@@ -332,6 +359,7 @@ void screenPrinter(char hangMan[8][11], char* tip, Word word, char* alreadyChoos
 		}
 	}
 
+	// prints the alrwady choosen letters, all upeer case, in a box 5 x 5
 	moveUp(7);
 	int alreadyChoosenLettersCounter = 0;
 	for (int count1 = 0; count1 < 6; count1++)
@@ -355,29 +383,33 @@ void screenPrinter(char hangMan[8][11], char* tip, Word word, char* alreadyChoos
 
 int main(int argc, char** argv)
 {
-	char letterGrid[3][5];
-	char hangMan[8][11];
-	char trashcan[10];
-	char alreadyChosenLetters[27];
-	int alreadyChosenLettersCounter = 0;
-	int hangManState = 0;
-	FILE* textFile;
-	Word word;
-	Word display;
+	char letterGrid[3][5]; // where the ascii art of the letter is stored
+	char hangMan[8][11]; // whre the hangman ascii art is stored
+	char trashcan[10]; // gets inputs by fgets, for menus, game inputs and pauses
+	char alreadyChosenLetters[27]; // letters that were already choose are stored here
+	int alreadyChosenLettersCounter = 0; // tracks how many letters were choosen. Used to manipulate the the alreadyChosenLetters array
+	int hangManState = 0; // tracks the hangman state
+	FILE* textFile; // the text file where the word and the tip is
+	Word word; // stores the word info
+	Word display; // stores the displayed word
 
+	// opens the file
 	textFile = fopen(argv[1], "r");
 	if (textFile == NULL)
 	{
-		printf("Text file not found, make sure it's named \"word.txt\" (without que quotes)\n");
+		printf("Text file couldn't be opened. Make sure it's passed by argument\n");
 		fgets(trashcan, 5, stdin);
 		return 1;
 	}
 
+	// sets the alreadyChosenLetters to be all spaces ("empty")
 	for (int count = 0; count < 27; count++)
 		alreadyChosenLetters[count] = ' ';
-
+	// sets the end of the alreadyChosenLetters already
 	alreadyChosenLetters[0] = '\0';
 
+	// verifyes if the file is valid
+	// also warns about missing word
 	if (fgets(word.word, 30, textFile) == NULL)
 	{
 		printf("The file is empty! Please insert an word\n");
@@ -388,13 +420,17 @@ int main(int argc, char** argv)
 	if (fgets(word.tip, 40, textFile) == NULL)
 		word.tip[0] = '\0';
 
-
+	// gets the word lenght
 	word.numberOfLetters = (strlen(word.word) - 1);
 
+	// creates the letterArray
 	letterCopier(letterGrid, 'a', true);
 	hangManCopyer(hangMan, HANG);
 
+	// creates the display word
 	display = word;
+
+	// set all character of the display word  to '_', except if it is '-'
 	for (int count = 0; count < word.numberOfLetters; count++)
 		display.word[count] = '_';
 	for (int count = 0; count < word.numberOfLetters; count++)
@@ -403,22 +439,41 @@ int main(int argc, char** argv)
 			display.word[count] = '-';
 	}
 
+	// play loop
 	for (bool end = false; end == false;)
 	{
-		screenPrinter(hangMan, display.tip, display, alreadyChosenLetters);
+		screenPrinter(hangMan, display.tip, display, alreadyChosenLetters); // prints the screen
 		puts("\n\nChoose a letter:");
 		fgets(trashcan, 5, stdin);
+
+		// puts the end character to work with strstr
 		trashcan[1] = '\0';
+
+		// converts upper case to lower case
 		if (trashcan[0] >= 'A' && trashcan[0] <= 'Z')
 			trashcan[0] += 32;
+
+		// looks if it's a valid letter
+		// if not the play loops
 		if (!((trashcan[0] >= 'A' && trashcan[0] <= 'Z') || (trashcan[0] >= 'a' && trashcan[0] <= 'z')))
 			continue;
+
+		// looks if the letter has already been choosen
+		// if yes, the game loops
 		if (strstr(alreadyChosenLetters, trashcan) != NULL)
 			continue;
+
+		// sees if the letter is in the word
+		// if not, it'll updates the hangman phase
 		if (displayModifyer(word, &display, trashcan[0]) == false)
 			hangManState++;
+
+		// updates the hangman state
 		hangManCopyer(hangMan, hangManState);
 		trashcan[1] = '\0';
+
+		// puts the choosen letter in the alreadyChosenLetters array
+		// and updates the alreadyChosenLettersCounter
 		if ((trashcan[0] >= 'a' && trashcan[0] <= 'z') && (strstr(alreadyChosenLetters, trashcan) == NULL))
 		{
 			alreadyChosenLetters[alreadyChosenLettersCounter] = trashcan[0];
@@ -426,8 +481,10 @@ int main(int argc, char** argv)
 			alreadyChosenLetters[alreadyChosenLettersCounter] = '\0';
 		}
 
+		// looks if the game has ended
 		if (endVerifyer(hangMan, display) == 1)
 		{
+			// prints the screen with victory
 			screenPrinter(hangMan, display.tip, display, alreadyChosenLetters);
 			printf("\nYou win!\n");
 			fgets(trashcan, 5, stdin);
@@ -435,6 +492,7 @@ int main(int argc, char** argv)
 		}
 		if (endVerifyer(hangMan, display) == 2)
 		{
+			// prints the screen with loss
 			screenPrinter(hangMan, word.tip, word, alreadyChosenLetters);
 			printf("\nYou Lose!\n");
 			fgets(trashcan, 5, stdin);
@@ -442,12 +500,13 @@ int main(int argc, char** argv)
 		}
 	}
 
+	//prints the thanks
 	puts("Thank You for playing!");
 
 	puts("Stickman art by Joan G. Stark (available at https://www.asciiart.eu/miscellaneous/noose)");
 	puts("Ascii art font by Patrick Gillespie (twiiter @patorjk) and his Text to ASCII Art Generator (http://patorjk.com/software/taag)");
 	printf("\n");
-	puts("This project is made possible by Notepad++ and Visual Studio Community 2019, shoutouts");
+	puts("This project is made possible by Notepad++ and Visual Studio Community 2019");
 	printf("\n");
 	puts("Made by Lucas Isaac (@LIPSilva5)");
 	fgets(trashcan, 5, stdin);
